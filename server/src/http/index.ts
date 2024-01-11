b9a30b5c8302c6dffe4477b6e222604dac0bfb85
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-11 20:21:29 -0300

    chore(server): setup @grotto/logysia

--- server/src/http/index.ts
+++ server/src/http/index.ts
@@ -1,7 +1,11 @@
-import { Elysia } from "elysia";
+import { logger } from '@grotto/logysia'
+import { Elysia } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(logger({ logIP: true }))
+  .get('/', () => 'Hello Elysia')
+  .listen(3000)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}\n`
+)
