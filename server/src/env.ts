0733f54b14b0da0e4433885c380e607a63d562c9
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-12 19:55:40 -0300

    feat(server): add JWT_SECRET_KEY environment variable

--- server/src/env.ts
+++ server/src/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   DB_URL: z.string().url().min(1),
+  JWT_SECRET_KEY: z.string().min(1),
 })
 
 export const env = envSchema.parse(process.env)
