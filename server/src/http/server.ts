f25f9d656d9973d9e4f6d7bafd0495be8da14c5c
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-12 20:02:23 -0300

    feat(server): add NOT_FOUND error handler on main server module

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,9 +1,22 @@
 import { Elysia } from 'elysia'
 import { logger } from '@grotto/logysia'
 
+import { NotFoundError } from './errors/not-found-error'
 import { routes } from './routes'
 
-const app = new Elysia().use(logger({ logIP: true })).use(routes)
+const app = new Elysia()
+  .use(logger({ logIP: true }))
+  .error({
+    NOT_FOUND: NotFoundError,
+  })
+  .onError(({ code, error, set }) => {
+    switch (code) {
+      case 'NOT_FOUND':
+        set.status = 404
+        return { code, message: error.message }
+    }
+  })
+  .use(routes)
 
 app.listen(3000)
 
