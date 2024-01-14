0e7639ef9270fca3af5b7bacf1bbb8007954ce1c
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-14 12:32:44 -0300

    feat(web): setup dark-theme

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
 
 import { cn } from '~/lib/utils'
+import { ThemeProvider } from '~/components/theme-provider'
+
 import './globals.css'
 
 export const fontSans = FontSans({
@@ -19,7 +21,14 @@ export default function RootLayout({
       <body
         className={cn('min-h-screen font-sans antialiased', fontSans.variable)}
       >
-        {children}
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
