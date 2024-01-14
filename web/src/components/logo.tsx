547e57ed1fdeba16727475615186b607fa2663e8
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-14 18:49:55 -0300

    feat(web): create `Logo` component

---
+++ web/src/components/logo.tsx
@@ -0,0 +1,8 @@
+import { Pizza } from 'lucide-react'
+
+export const Logo = () => (
+  <span className='inline-flex items-center gap-3 text-lg font-medium'>
+    <Pizza className='h-5 w-5' />
+    <span className='font-semibold'>pizza.shop</span>
+  </span>
+)
