87fee7dee2d78cda154cf12ce2b1544251b4fc57
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-22 21:02:43 -0300

    feat(web): add password verification on sign-in form

--- web/src/app/(auth)/sign-in/_lib/form-schema.ts
+++ web/src/app/(auth)/sign-in/_lib/form-schema.ts
@@ -2,6 +2,6 @@ import * as z from 'zod'
 
 export const formSchema = z.object({
   email: z.string().email('Email inválido'),
-  password: z.string(),
+  password: z.string().min(1, 'A senha não pode estar vazia'),
 })
 export type FormSchema = z.infer<typeof formSchema>
