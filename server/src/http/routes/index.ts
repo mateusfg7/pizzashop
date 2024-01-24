49ea89acf40ee052fab03ffc45702d847322b69a
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-01-23 22:14:38 -0300

    feat(server): create `/sign-out` route

--- server/src/http/routes/index.ts
+++ server/src/http/routes/index.ts
@@ -3,6 +3,7 @@ import Elysia from 'elysia'
 import { helloWorld } from './hello-world'
 import { registerCustomer } from './register-customer'
 import { signIn } from './sign-in'
+import { signOut } from './sign-out'
 import { profile } from './profile'
 import { registerRestaurant } from './register-restaurant'
 
@@ -10,5 +11,6 @@ export const routes = new Elysia()
   .use(helloWorld)
   .use(registerCustomer)
   .use(signIn)
+  .use(signOut)
   .use(profile)
   .use(registerRestaurant)
